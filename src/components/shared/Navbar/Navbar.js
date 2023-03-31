f24d1c243d28b236ca7a106d6c098bd298a4ef90
Pranil-Meherkar <pranil@nimapinfotech.com>
2023-03-31 15:03:47 +0530

    removed react scroll package and implemented scroll functionality

--- src/components/shared/Navbar/Navbar.js
+++ src/components/shared/Navbar/Navbar.js
@@ -2,16 +2,24 @@ import React from 'react'
 import { Link } from 'react-scroll'
 import './Navbar.css'
 
-const Navbar = () => {
+const Navbar = ({setActive}) => {
   return (
     <div className='main'>
         <nav className='nav'>
         <div className='site'>ABC</div>
         <ul>
-            <li><Link activeClass="active" to='/' spy={true} smooth={true} offset={-40} duration={500} >Home</Link></li>
-            <li><Link activeClass="active" to='/about' spy={true} smooth={true} offset={-40} duration={500}>About</Link></li>
-            <li><Link activeClass="active" to='/gallery' spy={true} smooth={true} offset={-40} duration={500}>Gallery</Link></li>
-            <li><Link activeClass="active" to='/contact' spy={true} smooth={true} offset={-40} duration={500}>Contact</Link></li>
+            <li onClick={()=> {
+              setActive("home")
+            }}>Home</li>
+            <li onClick={()=> {
+              setActive("about")
+            }}>About</li>
+            <li onClick={()=> {
+              setActive("gallery")
+            }}>Gallery</li>
+            <li onClick={()=> {
+              setActive("contact")
+            }}>Contact</li>
         </ul>
         </nav>
     </div>
